de7d85c3e6afddf861ca29e9d6f92743e3c09fec
Abdelaziz FATHALLAH <azizfathallah50@gmail.com>
2024-12-04 10:36:42 +0100

    Add Comments for all the Files

--- ConsoleApp_ShapeCalculator/Circle.cs
+++ ConsoleApp_ShapeCalculator/Circle.cs
@@ -9,11 +9,14 @@ namespace ConsoleApp_ShapeCalculator
     class Circle : Shape
     {
         private double _radius;
+
+        // Constructeur pour initialiser le rayon.
         public Circle(int radius) 
         {
             _radius = radius;
         }
 
+        // Implémentation de la méthode abstraite pour un cercle.
         public override double GetArea()
         {
             return Math.PI * _radius * _radius;
--- ConsoleApp_ShapeCalculator/Program.cs
+++ ConsoleApp_ShapeCalculator/Program.cs
@@ -6,11 +6,13 @@ namespace ConsoleApp_ShapeCalculator
     {
         static void Main(string[] args)
         {
-            Shape circle = new Circle(5);
-            circle.DisplayArea();
+            // Création d'une instance de Circle.
+            Shape circle = new Circle(5); // Rayon = 5
+            circle.DisplayArea(); // Affiche l'aire du cercle.
 
-            Shape rectangle = new Rectangle(3, 7);
-            rectangle.DisplayArea();
+            // Création d'une instance de Rectangle.
+            Shape rectangle = new Rectangle(3, 7); // Largeur = 3, Hauteur = 7
+            rectangle.DisplayArea(); // Affiche l'aire du rectangle.
         }
     }
 }
